fd4307f7ecb5a8dfe4aaeb36021dc7595f9df220
Joshua Gugun Siagian <suabahasa@gmail.com>
2024-07-21 00:41:21 +0700

    new ETL (with deno.js)

---
+++ etl/traffic.js
@@ -0,0 +1,138 @@
+import { 
+    parse as parseCsv,
+    stringify as toCsv,
+ } from "@std/csv";
+
+/**
+ * TASK: 0. Read the csv file and parse it
+ */
+
+// read the csv file
+const csvFile = await Deno.readTextFile('../00-traffic.csv');
+
+// parse the csv
+const records = parseCsv(csvFile, {
+    skipFirstRow: true,
+});
+
+const recordsHeader = Object.keys(records[0]);
+
+/**
+ * TASK: 1. Fill in the missing values with the mean of the column. Process the 'Timed Volume' columns (7-30)
+ */
+
+const columnTimedVolumeMean = {};
+
+// get the mean of each column
+for (let i = 7; i < 31; i++) {
+    const key = recordsHeader[i];
+    columnTimedVolumeMean[key] = records.reduce((acc, record) => {
+        return acc + (parseFloat(record[key]) || 0);
+    }, 0) / records.length;
+}
+
+// fill in the missing values with the mean of the column
+records.map((record) => {
+    for (let i = 7; i < 31; i++) {
+        const key = recordsHeader[i];
+        if (isNaN(parseFloat(record[key]))) {
+            record[key] = columnTimedVolumeMean[key];
+        }
+    }
+    return record;
+});
+
+/**
+ * TASK: 2. Group the columns by Time with 12-hour system (AM/PM), and calculate the average of the columns.
+ */
+
+records.map((record) => {
+    // get the mean of recordsHeader columns (7-18)
+    const timedVolumeAM = recordsHeader.slice(7, 19).reduce((acc, key) => {
+        return acc + parseFloat(record[key]);
+    }, 0) / 12;
+
+    // get the mean of recordsHeader columns (19-30)
+    const timedVolumePM = recordsHeader.slice(19, 31).reduce((acc, key) => {
+        return acc + parseFloat(record[key]);
+    }, 0) / 12;
+
+    record['AM'] = timedVolumeAM;
+    record['PM'] = timedVolumePM;
+
+    // format the date. Original format: MM/DD/YYYY, new format: YYYY-MM-DD
+    const dateParts = record['Date'].split('/');
+    record['Date'] = `${dateParts[2]}-${dateParts[0]}-${dateParts[1]}`;
+
+    return record;
+});
+
+
+/**
+ * TASK: 3. Group the record based on the Date and Roadway Name columns. If there are multiple records with the same date and roadway name, store the AM and PM values in an array with the key 'AM_collections' and 'PM_collections'
+ */
+
+const trafficRecords = {};
+
+records.forEach((record) => {
+    const key = `${record['Date']}_${record['Roadway Name']}`;
+    if (trafficRecords[key]) {
+        trafficRecords[key]['AM_collections'].push(record['AM']);
+        trafficRecords[key]['PM_collections'].push(record['PM']);
+    } else {
+        trafficRecords[key] = {
+            'Roadway Name': record['Roadway Name'],
+            Date: record['Date'],
+            AM: record['AM'],
+            PM: record['PM'],
+            AM_collections: [record['AM']],
+            PM_collections: [record['PM']],
+        };
+    }
+});
+
+
+/**
+ * TASK: 4. Convert the trafficRecords object to an array and sort it by date and roadway name
+ */
+
+const trafficRecordsArray = Object.values(trafficRecords);
+
+trafficRecordsArray.sort((a, b) => {
+    if (a.Date === b.Date) {
+        return a['Roadway Name'] < b['Roadway Name'] ? -1 : 1;
+    }
+
+    return a.Date < b.Date ? -1 : 1;
+});
+
+
+/**
+ * TASK: 5. Calculate the mean/average of the AM_collections and PM_collections if there are multiple records with the same date and roadway name
+ */
+
+// if the AM_collections and PM_collections have multiple records, calculate the mean and store it in the AM and PM properties
+trafficRecordsArray.forEach((record) => {
+    if (record['AM_collections'].length > 1) {
+        record['AM'] = record['AM_collections'].reduce((acc, value) => acc + value, 0) / record['AM_collections'].length;
+        delete record['AM_collections'];
+    }
+
+    if (record['PM_collections'].length > 1) {
+        record['PM'] = record['PM_collections'].reduce((acc, value) => acc + value, 0) / record['PM_collections'].length;
+        delete record['PM_collections'];
+    }
+});
+
+/**
+ * TASK: 6. Write the records to a csv file
+ */
+
+await Deno.writeTextFile('../01-traffic.csv', toCsv(trafficRecordsArray, {
+    columns: [
+        'Roadway Name',
+        'Date',
+        'AM',
+        'PM',
+    ]
+}));
---
+++ etl/weather.js
@@ -0,0 +1,125 @@
+import { 
+    parse as parseCsv,
+    stringify as toCsv,
+ } from "@std/csv";
+
+/**
+ * TASK: 0. Read the csv file and parse it
+ */
+
+// read the csv file
+const csvFile = await Deno.readTextFile('../00-weather.csv');
+
+// parse the csv
+const records = parseCsv(csvFile, {
+    skipFirstRow: true,
+});
+
+/**
+ * TASK: 1. Group the records by date and meridiem (AM/PM)
+ */
+
+// an object to store the weather history records that will be grouped by date and meridiem
+const weatherHistory = {};
+
+// on each record, group the records by date and meridiem
+records.forEach((record) => {
+    // Formatted Date: YYYY-MM-DD HH:mm:ss.SSS ±HHmm
+
+    // get the first 10 characters of the 'Formatted Date' property
+    const date = record['Formatted Date'].slice(0, 10);
+
+    // get the next 2 characters after a space of the 'Formatted Date' property
+    const meridiem = record['Formatted Date'].slice(11, 13) >= 12 ? 'PM' : 'AM';
+
+    // find the property in the weatherHistory object with the name: `${date}_${meridiem}`, if it exists, push the record to the 'records' array
+    const key = `${date}_${meridiem}`;
+    if (weatherHistory[key]) {
+        weatherHistory[key].records.push(record);
+    } else {
+        weatherHistory[key] = {
+            date,
+            meridiem,
+            records: [record],
+        };
+    }
+});
+
+/**
+ * TASK: 2. Convert the weatherHistory object to an array and sort it by date and meridiem
+ */
+
+// convert as array
+const weatherHistoryArray = Object.values(weatherHistory);
+
+// sort by date and meridiem
+weatherHistoryArray.sort((a, b) => {
+    if (a.date === b.date) {
+        return a.meridiem === 'AM' ? -1 : 1;
+    }
+
+    return a.date < b.date ? -1 : 1;
+});
+
+/**
+ * TASK: 3. Calculate the mean/average from the records array of each group
+ */
+
+weatherHistoryArray.forEach((item, index) => {
+    // get the most common Precip Type
+    const precipType = item.records.reduce((acc, record) => {
+        // If Some records have 'Precip Type' as 'null', define it based on the 'Summary' property
+        if (record['Precip Type'] === 'null') {
+            if (['Mostly Cloudy', 'Partly Cloudy', 'Overcast'].includes(record['Summary'])) {
+                record['Precip Type'] = 'Cloudy';
+            } else if (record['Summary'] === 'Clear') {
+                record['Precip Type'] = 'Clear';
+            } else if (record['Summary'] === 'Foggy') {
+                record['Precip Type'] = 'Fog';
+            }
+        }
+
+
+        if (record['Precip Type'] in acc) {
+            acc[record['Precip Type']] += 1;
+        } else {
+            acc[record['Precip Type']] = 1;
+        }
+
+        return acc;
+    }, {});
+
+    item.weather = {
+        'Precip Type': Object.keys(precipType).reduce((a, b) => (precipType[a] > precipType[b] ? a : b)).toLowerCase(),
+        'Temperature': item.records.reduce((acc, record) => acc + parseFloat(record['Temperature (C)']), 0) / item.records.length,
+        'Humidity': item.records.reduce((acc, record) => acc + parseFloat(record['Humidity']), 0) / item.records.length,
+        'Wind Speed': item.records.reduce((acc, record) => acc + parseFloat(record['Wind Speed (km/h)']), 0) / item.records.length,
+    };
+});
+
+/**
+ * TASK: 4. Write the records to a csv file
+ */
+
+await Deno.writeTextFile('../01-weather.csv', toCsv(weatherHistoryArray, {
+    columns: [
+        'date',
+        'meridiem',
+        {
+            header: 'Precip Type',
+            prop: ['weather', 'Precip Type'],
+        },
+        {
+            header: 'Temperature',
+            prop: ['weather', 'Temperature'],
+        },
+        {
+            header: 'Humidity',
+            prop: ['weather', 'Humidity'],
+        },
+        {
+            header: 'Wind Speed',
+            prop: ['weather', 'Wind Speed'],
+        },
+    ]
+}));
