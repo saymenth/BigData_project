0b0eeeb5bfabb7967d299ceeec98c3195c2ba2ba
Joshua Gugun Siagian <suabahasa@gmail.com>
2024-07-21 01:31:58 +0700

    etl pipeline

--- etl/main.js
+++ etl/main.js
@@ -12,5 +12,13 @@ const command2 = new Deno.Command(
     }
 );
 
+const command3 = new Deno.Command(
+    Deno.execPath(),
+    {
+        args: ['run', '--allow-read', '--allow-write', 'join.js'],
+    }
+);
+
 command1.outputSync();
 command2.outputSync();
+command3.outputSync();
